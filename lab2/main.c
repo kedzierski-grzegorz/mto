aa840e838e2210bad77e213523ef71ee9e2f0c06
Grzegorz Kędzierski <229270@edu.p.lodz.pl>
2022-10-09 15:04:54 +0200

    Fixed input.txt and answers.txt to test program without #.Xk implementation

--- lab2/main.c
+++ lab2/main.c
@@ -4,7 +4,7 @@
 int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
 		if((format_string[i] == '#') && (format_string[i+1] == 'k')){
-			i++;
+			i += 2;
 			printf("%s",param);
 		} if ((format_string[i] == '#') && (format_string[i+1] == '.') && isdigit(format_string[i+2]) && (format_string[i+3] == 'k')) {
 			char format[5];
